46bd877ba7f8ef737661241c4051ca40ba74363d
Anamika0246 <gorgeousanamika25@gmail.com>
2025-02-04 00:57:34 +0530

    Created Home page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Home from "../src/pages/home/Home.jsx"
+function App() {
+
+  return (
+    <>
+      <div className=' h-screen w-full'>
+      <Home/>
+
+      </div>
+    </>
+  )
+}
+
+export default App
